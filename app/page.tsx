0e7c8322ac673391f37541eae76c008c17e1926b
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-10-26 17:49:41 +0000

    Make profile picture responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,21 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.intro}>
-          <Image
-            src="/profile-pic.jpg"
-            width={300}
-            height={300}
-            alt="Portrait of the author"
-          />
+          <div className={styles.pfpContainer}>
+            <Image
+              className={styles.pfp}
+              src="/profile-pic.jpg"
+              // width={300}
+              // height={300}
+              fill
+              alt="Portrait of the author"
+            />
+          </div>
           <div>
             <h1>Hi, I'm Holly.</h1>
             <p>Recent computing graduate with a passion for developing software and web applications. Experienced in collaborative and independent projects, including full stack web technologies and embedded IoT systems. Eager to contribute to a development team and grow through industry experience.</p>
+            <p>.</p>
+            <p>Contact Me</p>
           </div>
         </div>
       </main>
