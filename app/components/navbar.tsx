7289e327de51f0fb957e08a992a6c3dd15fd3ea2
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-10-26 22:57:25 +0000

    Add transition to navbar and define global variables

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/resume">Resume</Link>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/resume">Resume</Link>
     </div>
   );
 }
