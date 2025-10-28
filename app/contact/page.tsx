8516e1c66bef5c8b0a8798adebde657f442e2c80
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-10-28 20:41:01 +0000

    Create contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,42 @@
+import Form from 'next/form'
+import styles from "./page.module.css";
+
 export default function Contact() {
+
+  const formAction = async (formData: FormData) => {
+
+    'use server'
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const message = formData.get("message");
+    console.log(name);
+    console.log(email);
+    console.log(message);
+  }
+
   return (
     <>
-    <h1>Contact Me</h1>
-    <p>hello world</p>
+      <h1>Contact Me</h1>
+      <p className={styles.desc}>Thanks for reaching out to me. You can contact me by my email at hollyjordan200202@gmail.com or by filling out this short form.</p>
+      <Form className={styles.form} action={formAction}>
+        <label>
+          Name
+          <input name="name" />
+        </label>
+
+        <label>
+          Email Address
+          <input name="email" />
+        </label>
+
+        <label>
+          Message
+          <textarea className={styles.message} name="message" />
+        </label>
+
+        <button type="submit">Submit</button>
+      </Form>
     </>
   );
 }
