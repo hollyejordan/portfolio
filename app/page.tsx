9ad05d6a5613ef2bc6ffdbc77ac8d655072e313d
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-10-29 23:51:46 +0000

    Add thumbnails and switch to featured projects

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,12 @@ export default function Home() {
       </div>
       
       <div className={styles.exploreSection}>
-        <h2>Take a look around</h2>
+        <h2>Featured Projects</h2>
         <div className={styles.exploreCards}>
-          <ExploreCard title="About" path="/about" thumbnail="" description="Learn more about who I am and what drives me."/>
-          <ExploreCard title="Projects" path="/projects" thumbnail="/projects-thumbnail-60.png" description="Explore some of the projects I've built and what I've learned from them."/>
-          <ExploreCard title="Resume" path="/resume" thumbnail="" description="See my experience, skills and education."/>
-          <ExploreCard title="Contact" path="/contact" thumbnail="" description="I'd love to hear from you - feel free to reach out with any questions!"/>
+          <ExploreCard title="Houseplant Monitoring System" path="/projects/houseplant-monitoring-system" thumbnail="/houseplant-thumbnail-16-9.png" description="A full-stack MERN web app that integrates with an ESP microcontroller to visualise and monitor live plant sensor data."/>
+          <ExploreCard title="Live Transcription Smart Glasses" path="/projects" thumbnail="/glasses-thumbnail-16-9.jpg" description="An ESP microcontroller system that projects live transcriptions of audio before your eyes, housed in a 3D printed frame."/>
+          <ExploreCard title="Food Waste Management App" path="/contact" thumbnail="/useby-thumbnail-16-9.jpg" description="A React Natvie app for tracking food expiration dates and reducing food waste."/>
+          <ExploreCard title="Arduino Experiments Portfolio" path="/resume" thumbnail="/experiments-thumbnail-16-9.jpg" description=""/>
         </div>
       </div>
     </>
