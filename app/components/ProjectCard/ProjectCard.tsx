5da40021274641baee9e436c09692aee7060d938
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-11-07 12:35:51 +0000

    Create ProjectCard component

---
+++ app/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import styles from "./ProjectCard.module.css"
+
+type ProjectCardProps = {
+}
+
+export default function ExploreCard({ }: ProjectCardProps) {
+
+  return (
+    <div>
+        <p>project card</p>
+    </div>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+import ProjectCard from "../components/ProjectCard/ProjectCard";
+import styles from "./page.module.css";
+
+export default function Projects() {
+
+  return (
+    <>
+      <h1>Projects</h1>
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </>
+  );
+}
