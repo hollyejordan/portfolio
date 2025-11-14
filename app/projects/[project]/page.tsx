dad1e95031b7dfe107d20c4c19464291c1a81289
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-11-14 22:35:58 +0000

    Add HouseplantWebsite component

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,6 +1,8 @@
 import getData from "@/app/lib/getData";
 import Image from "next/image";
 import styles from "./page.module.css";
+import HouseplantWebsite from "../project_components/HouseplantWebsite";
+import { notFound } from "next/navigation";
 
 type ProjectProps = {
   params: Promise<{ project: string }>;
@@ -12,7 +14,17 @@ export default async function Project({ params }: ProjectProps) {
   const data = await getData();
   const projectData = data[project];
 
-  return <>
+  const components: Record<string, React.FC> = {
+    "houseplant-monitoring-system": HouseplantWebsite
+  };
+
+  const SelectedComponent = components[project];
+
+  if (!SelectedComponent) {
+    notFound();
+  }
+
+  return <div className={styles.projectBody}>
     <div className={styles.banner}>
       <Image
         src={projectData.thumbnail}
@@ -25,7 +37,6 @@ export default async function Project({ params }: ProjectProps) {
         <p>{projectData.date}</p>
       </div>
     </div>
-    <h2>Introduction</h2>
-    <p>{projectData.description}</p>
-  </>
+    <SelectedComponent />
+  </div>
 }
---
+++ app/projects/project_components/HouseplantWebsite.tsx
@@ -0,0 +1,12 @@
+import styles from "app\projects\[project]\page.module.css";
+
+export default function HouseplantWebsite() {
+    return (
+    <>
+        <h2>Introduction</h2>
+        <p>A full-stack MERN (MongoDB, Express, React, Node.js) web application that integrates with an ESP32 microcontroller to monitor and visualise live plant sensor data. The ESP32 periodically takes light and soil moisture readings and sends them via Wi-Fi to the server, for storage in the database. The front end displays a dashboard overview of each plant and evaluates its status based on the current sensor data.</p>
+        <h2>Website Demonstration Video</h2>
+        <iframe src="https://www.youtube.com/embed/uN9Mb3KPpdo?si=mEKfUDj2oj7XVIlN" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </>
+    )
+}
