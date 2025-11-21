f52f4fca3b750aa7fd1337932454f49bbb5a1106
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-11-21 22:31:30 +0000

    Add solution section to houseplant project

--- app/projects/project_components/HouseplantWebsite.tsx
+++ app/projects/project_components/HouseplantWebsite.tsx
@@ -2,7 +2,7 @@ export default function HouseplantWebsite() {
     return (
     <>
         <h2>Introduction</h2>
-        <p>A full-stack MERN (MongoDB, Express, React, Node.js) web application that integrates with an ESP32 microcontroller to monitor and visualise live plant sensor data. The ESP32 periodically takes light and soil moisture readings and sends them via Wi-Fi to the server, for storage in the database. The front end displays a dashboard overview of each plant and evaluates its status based on the current sensor data.</p>
+        <p>A full-stack MERN (MongoDB, Express, React, Node.js) web application that integrates with an ESP32 Feather microcontroller to monitor and visualise live plant sensor data. The ESP32 periodically takes light and soil moisture readings and sends them via Wi-Fi to the server, for storage in the database. The front end displays a dashboard overview of each plant and evaluates its status based on the current sensor data.</p>
         <h2>Website Demonstration</h2>
         <iframe src="https://www.youtube.com/embed/uN9Mb3KPpdo?si=mEKfUDj2oj7XVIlN" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         <h2>Background</h2>
@@ -17,6 +17,7 @@ export default function HouseplantWebsite() {
             <li>Interpreting plant feedback, which is often slow or ambiguous, such as yellowing leaves</li>
         </ul>
         <h3>The Solution</h3>
+        <p>To address these issues, this project provides users with a clear way to monitor all of their houseplants. The dashboard presents overviews of each plant, so the user can quickly identify any issues or needs. Intuitive status icons make plant feedback unambiguous, and through using real-time sensor data, the user can understand the underlying conditions in their home and identify the source of any issues. Users can take a more detailed look at any of their plants by visiting a plant's profile, which provides further information and sensor-based metrics.</p>
         <h2>Features</h2>
         <ul>
             <li>Dashboard overview displaying all plants with real-time status icons</li>
@@ -27,6 +28,8 @@ export default function HouseplantWebsite() {
             <li>Forms to add new plants or edit existing information</li>
         </ul>
         <h2>System Architecture</h2>
+        <h3>Frontend</h3>
+        <p></p>
         <h2>Links</h2>
         <ul>
             <li>
