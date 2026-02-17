28947776403eff4d6d93a0f86c233b6fc2fc8381
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2026-02-17 15:41:27 +0000

    Create return buttons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,24 @@
+"use client"
 import LinkButton from "../components/LinkButton/LinkButton";
+import { useRouter } from 'next/navigation'
+import styles from "./page.module.css";
 
 export default function Contact() {
+
+    const router = useRouter();
+
+    const back = () => {
+        router.back();
+    }
 
   return (
     <>
         <h1>About</h1>
-        <p>This website's development is still in progress. This page has not been fully implemented yet. Thank you for your patience.</p>
-        <LinkButton path="/" innerText="Return To Homepage"></LinkButton>
+        <p>This website is still in development. This page has not been fully implemented yet. Thank you for your patience.</p>
+        <div className={styles.returnButtonContainer}>
+            <LinkButton path="/" innerText="Return To Homepage"></LinkButton>
+            <a className="button" onClick={back}>Return To Previous Page</a>
+        </div>
     </>
   );
 }
