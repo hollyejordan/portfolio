01e6ff440d33f3741cd51f832f7d5b1b46adc131
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2026-02-17 16:18:02 +0000

    Create resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import LinkButton from "../components/LinkButton/LinkButton";
+import { useRouter } from 'next/navigation'
+import styles from "./page.module.css";
+
+export default function Contact() {
+
+    const router = useRouter();
+
+    const back = () => {
+        router.back();
+    }
+
+  return (
+    <>
+        <h1>Resume</h1>
+        <p>This website is still in development. This page has not been fully implemented yet. Thank you for your patience.</p>
+        <div className={styles.returnButtonContainer}>
+            <LinkButton path="/" innerText="Return To Homepage"></LinkButton>
+            <a className="button" onClick={back}>Return To Previous Page</a>
+        </div>
+    </>
+  );
+}
