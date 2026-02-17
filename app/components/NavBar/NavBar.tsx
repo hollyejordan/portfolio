e728cf8034c3b9bde55b87855ad9cfef25dfea33
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2026-02-17 14:33:50 +0000

    Make navbar responsive to screen size

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className={styles.navbar}>
+    <div className={styles.navContainer}>
+      <div className={styles.navbar}>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/projects">Projects</Link>
         <Link href="/resume">Resume</Link>
         <Link href="/contact">Contact Me</Link>
     </div>
+    </div>
+    
   );
 }
