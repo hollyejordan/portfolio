933c33e98e301d80149847a4f3affbebe3d73fc5
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2026-02-17 15:23:38 +0000

    Create /about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import LinkButton from "../components/LinkButton/LinkButton";
+
+export default function Contact() {
+
+  return (
+    <>
+        <h1>About</h1>
+        <p>This website's development is still in progress. This page has not been fully implemented yet. Thank you for your patience.</p>
+        <LinkButton path="/" innerText="Return To Homepage"></LinkButton>
+    </>
+  );
+}
